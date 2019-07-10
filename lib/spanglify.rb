dfde7a57e3fc85c6eceeab201999f91b70a87810
Stephanie Fischler <sjfischler@gmail.com>
2019-07-10 11:42:40 -0400

    Update functionality

--- lib/spanglify.rb
+++ lib/spanglify.rb
@@ -2,18 +2,35 @@ require "spanglify/version"
 
 module Spanglify
   def self.process(str)
-		str = str.downcase
+		#commonly interchanged English to Spanish words and phrases
 		to_swap = {
 			"are you" => "estás",
 			"very" => "muy",
 			"but" => "pero",
-			"I like" => "me gusta",
+			"i like" => "me gusta",
 			"party" => "fiesta",
-			"with" => "con",
-			"to check" => "chequear"
+			"with" => "con"
 		}
 
-		spanglish = to_swap.inject(str) {|str, (k, v)| str.gsub(k,v)}
-		spanglish.capitalize
+		to_swap.each do |key, value|
+		#create a capitalized version of the key and values
+					capitalized_key = key.capitalize
+					capitalized_value = value.capitalize
+
+					#if the string contains the lowercase version of the English word(s)
+					#swap out for the lowercase Spanish version of the word(s)
+					if str.include? "#{key}"
+						str.gsub!(key, value)
+
+					#else if the string contains the capitalized version of the English word(s)
+					#swap out for the capitalized Spanish version of the word(s)
+					elsif str.include? "#{capitalized_key}"
+						str.gsub!(capitalized_key, capitalized_value)
+					else
+						next
+					end
+				end
+				#return the spanglified version of the string
+				return str
+			end
 	end
-end
--- spec/spanglify_spec.rb
+++ spec/spanglify_spec.rb
@@ -1,14 +1,15 @@
 RSpec.describe Spanglify do
-	let(:input) { 'Are you ready?' }
-	let(:output) { subject.process(input) }
 
 	describe "#process" do
-		it 'swaps an English word for a Spanish word' do
-			expect(output).to eq('Estás ready?')
+		let(:input) { 'i like to party, but I do not like to Dance.' }
+		let(:output) { subject.process(input) }
+
+		it 'swaps the appropriate English word or phrase for the matching Spanish word or phrase' do
+			expect(output).to eq('me gusta to fiesta, pero I do not like to Dance.')
 		end
 
 		it 'swaps words keeping appropriate capitalization' do
-			expect(output).not_to eq('estás ready?')
+			expect(output).not_to eq('Me gusta to fiesta, pero I do not like to dance.')
 		end
 	end
 end
